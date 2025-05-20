22b7ca46899aba1f6405de2daf38783f512be92a
pix <tpxsky@163.com>
2025-05-20 23:08:16 +0800

    <add:>add caddy install

---
+++ caddy_install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+COLOR_GREEN='\033[0;32m'
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_NC='\033[0m'
+
+if command -v caddy &>/dev/null; then
+  echo -e "${COLOR_GREEN}caddy is installed $COLOR_NC"
+else
+  echo -e "${COLOR_YELLOW}caddy is not installed $COLOR_NC"
+  sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+  sudo apt update
+  sudo apt install caddy
+fi
