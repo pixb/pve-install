ba153178f31fc61a3f122aff6d7453a3795c66d9
root <root@pve.3855u>
2025-09-20 00:14:29 +0800

    <modify:>modify pve_6_to_7.sh, Prepare before changing to the pve6to7 command.

--- pve_6_to_7.sh
+++ pve_6_to_7.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-if [ ! -e /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg ]; then
-  wget http://mirrors.ustc.edu.cn/proxmox/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
-fi
-
-if [ ! -e /etc/apt/sources.list.d/pve-install-repo.list ]; then
-  echo "deb http://mirrors.ustc.edu.cn/proxmox/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-fi
-
-if [ ! -e /etc/apt/sources.list.d/pve-no-subscription.list.bak ]; then
-  mv /etc/apt/sources.list.d/pve-no-subscription.list /etc/apt/sources.list.d/pve-no-subscription.list.bak
-fi
-
-if [ -e /etc/apt/sources.list.d/ceph.list ]; then
-  mv /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.bak
+# if [ ! -e /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg ]; then
+#   wget http://mirrors.ustc.edu.cn/proxmox/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
+# fi
+# 
+# if [ ! -e /etc/apt/sources.list.d/pve-install-repo.list ]; then
+#   echo "deb http://mirrors.ustc.edu.cn/proxmox/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+# fi
+# 
+# if [ ! -e /etc/apt/sources.list.d/pve-no-subscription.list.bak ]; then
+#   mv /etc/apt/sources.list.d/pve-no-subscription.list /etc/apt/sources.list.d/pve-no-subscription.list.bak
+# fi
+# 
+# if [ -e /etc/apt/sources.list.d/ceph.list ]; then
+#   mv /etc/apt/sources.list.d/ceph.list /etc/apt/sources.list.d/ceph.list.bak
+# fi
+BULLSEYE_DEBIAN="deb http://security.debian.org/debian-security bullseye-security main contrib"
+if grep -e "${BULLSEYE_DEBIAN}" /etc/apt/sources.list > /dev/null; then
+echo "bullseye is exists"
+else
+echo "bullseye is not exists."
+echo "${BULLSEYE_DEBIAN}" >> /etc/apt/sources.list
 fi
