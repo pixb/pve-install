37093a3df2129f3154f5f3d8d85672b71ecafacd
pix <tpxsky@163.com>
2025-09-23 13:55:54 +0800

    <modify:>move script to install

---
+++ install/pve7_source_config.sh
@@ -0,0 +1,43 @@
+#!/bin/env bash
+COLOR_GREEN='\033[0;32m'
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_NC='\033[0m'
+
+cat <<<_EOF_
+"${COLOR_GREEN}GREEN${COLOR_NC}"
+"${COLOR_YELLOW}YELLOW${COLOR_NC}"
+"${COLOR_RED}RED${COLOR_NC}"
+_EOF_
+
+# gpg
+# backup gpg
+if [ ! -e /etc/apt/trusted.gpg.d.bak.ori ]; then
+  echo -e "${COLOR_YELLOW}trusted.gpg.d.bak.ori don't exists, create it.${COLOR_NC}"
+  cp -r /etc/apt/trusted.gpg.d /etc/apt/trusted.gpg.d.bak.ori
+fi
+
+# download gpg
+wget https://mirrors.ustc.edu.cn/proxmox/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+
+APT_SOURCES_DIR="/etc/apt/sources.list.d"
+USTC_CEPH_SOURCE="deb https://mirrors.ustc.edu.cn/proxmox/debian/ceph-pacific bullseye main"
+USTC_ENTERPRISE_SOURCE="deb https://mirrors.tuna.tsinghua.edu.cn/proxmox/debian bullseye pve-no-subscription"
+
+# ceph source
+if [ ! -e ${APT_SOURCES_DIR}/ceph.list.bak ]; then
+  echo -e "${COLOR_YELLOW}ceph.list backup file don't exists. create it.${COLOR_NC}"
+  mv ${APT_SOURCES_DIR}/ceph.list ${APT_SOURCES_DIR}/ceph.list.bak
+else
+  echo -e "${COLOR_GREEN} ceph.list backup file exists. configure new ceph.list${COLOR_NC}"
+fi
+echo "${USTC_CEPH_SOURCE}" >${APT_SOURCES_DIR}/ceph.list
+
+# pve enterprise source
+if [ ! -e ${APT_SOURCES_DIR}/pve-enterprise.list.bak ]; then
+  echo -e "${COLOR_YELLOW}pve-enterprise.list backup file don't exists. create it.${COLOR_NC}"
+  mv ${APT_SOURCES_DIR}/pve-enterprise.list ${APT_SOURCES_DIR}/pve-enterprise.list.bak
+else
+  echo -e "${COLOR_GREEN}pve-enterprise.list backup file exists. configure new ceph.list${COLOR_NC}"
+fi
+echo "${USTC_ENTERPRISE_SOURCE}" >${APT_SOURCES_DIR}/pve-enterprise.list
